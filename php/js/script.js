559d9e0ee3a908650aa51b282c808ac0ded6b014
Agustin <aguspinto08@gmail.com>
2025-10-01 17:30:13 -0300

    libritos

--- php/js/script.js
+++ php/js/script.js
@@ -1,20 +1,28 @@
 function clickbook(){
     const p = document.createElement('p')
-    p.textContent = 'asd'
+    p.textContent = 'Librito epico'
     
     const div = document.createElement('div')
     div.id = 'bookdiv'
 
+    div.addEventListener('click', (e) => e.stopPropagation());
+
     const span = document.createElement('span')
     span.id = 'bookspan'
+    span.onclick = function closespan(){
+            const div = document.getElementById("bookdiv");
+            div.style.animation = 'moveright 0.3s ease-out forwards';
+            const span = document.getElementById("bookspan")
+            span.style.animation = 'disapear 0.3s ease-out forwards';
+            setTimeout(() => {
+                span.remove();
+            }, 300)
+        }
 
     div.appendChild(p)
     span.appendChild(div)
-
     document.body.appendChild(span);
-// overlaySpan.remove();
 }
-
-function writeInDB(){
+function register(){
     
 }
